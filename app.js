50772a0a21725ebf478cdd88990f802be6f0b85d
Israel Akinwumi <israelakin14@gmail.com>
2022-05-31 01:35:10 +0800

    Implemented dice rolling logic and appropriate dice display

--- app.js
+++ app.js
@@ -8,3 +8,49 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer, diceRoller;
+
+scores = [0, 0]
+roundScore = 0;
+activePlayer = 0;
+diceRoller = document.querySelector('.btn-roll')
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+
+diceRoller.addEventListener('click', function () {
+    // Generate random number
+    var randomDIceNUmber = Math.floor(Math.random() * 6) + 1;
+
+    // Display result
+    document.querySelector('#current-' + activePlayer).textContent = randomDIceNUmber
+    var Dice = document.querySelector('.dice')
+    Dice.src = 'imgs/dice-' + randomDIceNUmber + '.png'
+    Dice.style.display = "flex"
+
+    // Update the round score IF the rolled number was NOT a 1
+    if (randomDIceNUmber !== 1) {
+        // Add score
+        roundScore += randomDIceNUmber
+        document.querySelector('#current-' + activePlayer).textContent = roundScore
+    }
+    else {
+        // Hide Dice
+        Dice.style.display = "none"
+
+        // Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+
+        // resetting game dependacies
+        roundScore = 0
+        document.getElementById('current-0').textContent = '0'
+        document.getElementById('current-1').textContent = '0'
+
+        // Toggling Active class for players
+        document.querySelector('.player-1-panel').classList.toggle('active')
+        document.querySelector('.player-2-panel').classList.toggle('active')
+    }
+})
